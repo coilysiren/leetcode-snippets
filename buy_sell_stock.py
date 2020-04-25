a029651b245200c0b0b0b5faad038628e5de112e
Lynn <firemagelynn@gmail.com>
2020-04-25 09:17:26 -0700

    cleanup

--- buy_sell_stock.py
+++ buy_sell_stock.py
@@ -5,9 +5,6 @@ class Solution:
     def maxProfit(self, prices: List[int]) -> int:
         maxProfit = 0
 
-        if len(prices) <= 1:
-            return maxProfit
-
         for i, buyPrice in enumerate(prices):
             for sellPrice in prices[i + 1 :]:
                 maxProfit = max(maxProfit, sellPrice - buyPrice)
