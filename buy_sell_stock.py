8f15c152a91366f5f616f7bc137917ebb690775c
Lynn <firemagelynn@gmail.com>
2020-04-25 09:15:09 -0700

    buy sell stock brute force

--- buy_sell_stock.py
+++ buy_sell_stock.py
@@ -1,1 +1,15 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock/
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        maxProfit = 0
+
+        if len(prices) <= 1:
+            return maxProfit
+
+        for i, buyPrice in enumerate(prices):
+            for sellPrice in prices[i + 1 :]:
+                maxProfit = max(maxProfit, sellPrice - buyPrice)
+
+        return maxProfit
