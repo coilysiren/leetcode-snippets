a2d3d4f3408164f46286eef1d9446a556da3aa54
Lynn <firemagelynn@gmail.com>
2020-04-25 08:50:09 -0700

    add example to replicate

--- max_profit.py
+++ max_profit.py
@@ -1,5 +1,7 @@
 # https://leetcode.com/problems/best-time-to-buy-and-sell-stock-ii/submissions/
 
+# return sum(prices[a+1] - prices[a] for a in range(len(prices)-1) if prices[a] < prices[a+1])
+
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
